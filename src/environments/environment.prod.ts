bf36ca392e00a2d6c1cff18d45ff02eeec318506
Markus Kollers <m.kollers@vertical.de>
2018-06-05 21:03:05 +0200

    Added config for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCn1aRnKJoE0vQpCk6Zt7ZZFuo-L-AeKAI',
+    authDomain: 'knnl-43fbb.firebaseapp.com',
+    databaseURL: 'https://knnl-43fbb.firebaseio.com',
+    projectId: 'knnl-43fbb',
+    storageBucket: 'knnl-43fbb.appspot.com',
+    messagingSenderId: '267545791501'
+  }
 };
