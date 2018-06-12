3d0c882bbee5a1c0bc2accf4a0fddcbd2d434643
Markus Kollers <m.kollers@vertical.de>
2018-06-12 22:55:54 +0200

    Added loader while navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of, Subscription } from 'rxjs';
+import { switchMap, map, filter, delay, debounce } from 'rxjs/operators';
 
 import { AuthService } from './shared/auth/services/auth.service';
 import { User } from './shared/data-access/models/user';
 import { UserService } from './shared/data-access/services/user.service';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,21 +16,33 @@ import { UserService } from './shared/data-access/services/user.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  isRouting$: Observable<boolean>;
   user$: Observable<User>;
-  test$: Observable<any>;
 
   constructor(
     private _changeDetectorRef: ChangeDetectorRef,
     private _iconRegistry: MatIconRegistry,
     private _sanitizer: DomSanitizer,
     authService: AuthService,
+    router: Router,
     userService: UserService
   ) {
+    this.registerIcons();
     this.user$ = authService.authState$.pipe(
       switchMap(user => !user ? of(null) : userService.getByUid(user.uid))
     );
 
-    this.registerIcons();
+    this.isRouting$ = router.events.pipe(
+      filter(event => event instanceof NavigationStart || event instanceof NavigationEnd),
+      map(event => {
+        if (event instanceof NavigationStart) {
+          return true;
+        } else if (event instanceof NavigationEnd) {
+          return false;
+        }
+        this._changeDetectorRef.markForCheck();
+      })
+    );
   }
 
   /** Registers all required svg-icons for the whole application */
