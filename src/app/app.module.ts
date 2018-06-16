bfe30b3096bd151ff73083c301d975df18204d7d
Markus Kollers <m.kollers@vertical.de>
2018-06-17 01:24:04 +0200

    Added sentry error tracking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { OverlayModule } from '@angular/cdk/overlay';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
+import * as Raven from 'raven-js';
 
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,18 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './shared/auth/auth.module';
 import { DataAccessModule } from './shared/data-access/data-access.module';
 import { LayoutModule } from './shared/layout/layout.module';
+
+Raven
+  .config('https://d0d205afba434a84969b0c6f6741238f@sentry.io/1227633')
+  .install();
+
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err: any): void {
+    if (environment.production) {
+      Raven.captureException(err.originalError || err);
+    }
+  }
+}
 
 @NgModule({
   declarations: [
