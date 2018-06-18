490fd7476763de28bf72c0abd569ebd5bf301d7e
Markus Kollers <mail@markuskollers.de>
2018-06-18 18:52:52 +0200

    Init user table

--- src/app/shared/user/components/user-table/user-table.component.ts
+++ src/app/shared/user/components/user-table/user-table.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { MatPaginatorIntl, MatTableDataSource } from '@angular/material';
+import * as _ from 'lodash';
+import { Subscription } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+import { User } from '../../../data-access/models/user';
 
 @Component({
   selector: 'knnl-user-table',
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.css']
 })
-export class UserTableComponent implements OnInit {
+export class UserTableComponent implements OnChanges, OnDestroy {
+  private _subscriptions: Subscription[] = [];
+  dataSource = new MatTableDataSource<User>();
+  displayedColumns: string[];
 
-  constructor() { }
+  @Input('knnl-users') users: User[];
 
-  ngOnInit() {
+  constructor(
+    private _changeDetectorRef: ChangeDetectorRef,
+    breakpointObserver: BreakpointObserver,
+    paginatorIntl: MatPaginatorIntl
+  ) {
+    paginatorIntl.getRangeLabel = (page, pageSize, length) => `${page + 1} von ${Math.ceil(length / pageSize)}`;
+    paginatorIntl.nextPageLabel = 'Nächste Seite';
+    paginatorIntl.previousPageLabel = 'Vorherige Seite';
+    paginatorIntl.itemsPerPageLabel = `Benutzer pro Seite`;
+
+    // Change display columns depending on screen size
+    this._subscriptions.push(
+      breakpointObserver.observe('(max-width: 839px)').pipe(
+        map(state => state.matches),
+        tap(isSmall => this.changeDisplayColumns(isSmall))
+      ).subscribe()
+    );
+  }
+
+  ngOnChanges() {
+    if (this.users) {
+      const data = _.orderBy(this.users, u => u.email);
+      this.dataSource.data = data;
+      this._changeDetectorRef.markForCheck();
+    }
+  }
+
+  /** Unsubscribes all subscriptions to avoid memory leaks */
+  ngOnDestroy() {
+    this._subscriptions.forEach(s => s.unsubscribe());
+  }
+
+  /** Changes visible columns depending on the screen size */
+  private changeDisplayColumns(isSmall: boolean) {
+    if (isSmall) {
+      this.displayedColumns = ['firstname', 'lastname'];
+    } else {
+      this.displayedColumns = ['email', 'firstname', 'lastname'];
+    }
+    this._changeDetectorRef.markForCheck();
   }
 
 }
