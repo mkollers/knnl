ef1938da686efb756437803353b769664a3d7c02
Markus Kollers <mail@markuskollers.de>
2018-06-29 19:22:10 +0200

    Temp checkin

--- src/app/shared/auth/services/auth.service.ts
+++ src/app/shared/auth/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { User } from 'firebase/app';
 import { Observable } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +12,17 @@ export class AuthService {
   authState$: Observable<User>;
 
   constructor(
-    private _auth: AngularFireAuth
+    private _auth: AngularFireAuth,
+    db: AngularFireDatabase
   ) {
     this.authState$ = _auth.authState;
+
+    this.authState$.pipe(
+      filter(user => !!user),
+      switchMap(user => db.object<number>(`metadata/${user.uid}/refreshTime`).valueChanges()),
+      tap(console.log),
+      tap(() => this._auth.auth.currentUser.getIdToken())
+    ).subscribe();
   }
 
   register(mail: string, password: string) {
